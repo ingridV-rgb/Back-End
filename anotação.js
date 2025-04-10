089be19a05607ccd47cf04f71f959560e947acc1
ingridV-rgb <ingrid.aluna2024@gmail.com>
2025-04-10 11:37:37 -0400

    atividade operadores

---
+++ anotação.js
@@ -0,0 +1,33 @@
+//operadores
+
+//aritmético +, -, /, * (adição, subtração, divisão, multiplicação) dá pra concatenar as strings com + --> converte todos os valores pra string e depois une
+
+// se somar 5 + 5 você terá 10. Se você somar 5 + true você vai ter 6, pois true vale 1
+// false = 0
+//true = 1
+
+//operadores de atribuição: simplificação x = y
+//atribuição de adição: x += y, seria a mesma coisa que: x = x + y, dá pra fazer com todos os operadores, é um jeito de simplificar
+
+// incremento = ++
+// decremento = --
+// são usados para adicionar ou subtrair o valor 1 de uma variável (mais usados em bases de repetição) --> contador++; console.log(contador) #se o contador valer 2, ele vai passar a valer 3
+// podem ser colocados antes ou após o nome da variável: antes = pré-incremento primeiro é incrementado e depois impresso na tela
+// colocar = pós-incremento primeiro é impresso depois é incrementado == isso se aplica ao -- também
+
+//operadores relacionais: (===) estritamente verdadeiro; retorna verdadeiro caso os operadores sejam iguais ou do mesmo tipo; tipo é igual e se o conteúdo é igual; dará erro se você colocar "21" (string) === 21 (int); dará erro, pois seus tipos são diferentes
+// (==) apenas verifica se os valores são iguais
+// (--) apenas verifica se os valores são diferentes
+// (!==) verifica os dois tipos, se o conteúdo é diferente e se o conteúdo é diferente; é praticamente ao estritamento verdadeiro, só que em false
+
+//undefined = não definido
+//null = sem valor
+
+//operadores lógicos: AND lógico (&&) retorna verdadeiro ambos operadores sejam verdadeiros
+//OU lógico (||) só retorna verdadeira se as duas opções forem falsas, caso uma das opções forem falsas retornará false #oposto do AND
+// NOT (!)
+
+// let idade = 25;
+// let mensagem = idade >= 18 ? "acesso permitido" : "acesso negado" #if e else simples
+
+// let: o valor pode ser alterado
---
+++ atividade_operadores/exercicios.js
@@ -0,0 +1,32 @@
+//atividade 1: Crie um programa que declare duas variáveis e exiba o resultado da soma, subtração, multiplicação e divisão desses números.
+
+
+let numero = 5
+let numero2 = 10
+
+console.log(numero+numero2);
+console.log(numero-numero2);
+console.log(numero*numero2);
+console.log(numero/numero2);
+
+//atividade 2: Declare duas idades e utilize operadores de comparação para verificar se uma pessoa é mais velha que a outra.
+
+let idade = 17
+let idade2 = 87
+
+console.log(idade>idade2);
+console.log(idade<idade2);
+
+//atividade 3: Crie uma variável idade com um número e use o operador ternário para verificar se o valor armazenado na variável é maior ou igual a 18. Imprima “Maior de idade” ou “Menor de idade”
+
+let idad = 3;
+
+let barragem = idad >= 18 ? "maior de idade" : "menor de idade";
+console.log(barragem);
+
+//atividade 4: Crie uma variável com um número e use o operador ternário para verificar se é par ou ímpar.
+
+let nm = 50;
+
+let ababa = nm%2 == 0? "número par" : "número ímpar";
+console.log(ababa);
